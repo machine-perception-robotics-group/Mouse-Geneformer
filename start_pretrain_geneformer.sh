6fe3814d2583077392ee8cb83ad0221269580bfa
ItoKeita21 <itokeita@mprg.cs.chubu.ac.jp>
2024-09-06 16:24:13 +0900

    add file and change content

--- start_pretrain_geneformer.sh
+++ start_pretrain_geneformer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PATH=$PATH:~/.local/bin
-deepspeed --num_gpus=8 pretrain_geneformer_w_deepspeed_one_dataset_ver.py --deepspeed ds_config.json
+deepspeed --num_gpus=8 pretrain_geneformer.py --deepspeed ds_config.json
